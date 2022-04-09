285e36abc27c6d56f0b8337154fb303d8892bcc8
Prateek-Prakash <63609044+Prateek-Prakash@users.noreply.github.com>
2022-04-09 18:02:34 -0400

    Add Simple CalcService

--- lib/main.dart
+++ lib/main.dart
@@ -217,3 +217,29 @@ class CalcKey extends StatelessWidget {
     );
   }
 }
+
+class CalcService {
+  static final CalcService _instance = CalcService._internal();
+
+  factory CalcService() {
+    return _instance;
+  }
+
+  CalcService._internal();
+
+  double add(double a, double b) {
+    return a + b;
+  }
+
+  double subtract(double a, double b) {
+    return a - b;
+  }
+
+  double multiply(double a, double b) {
+    return a * b;
+  }
+
+  double divide(double a, double b) {
+    return a / b;
+  }
+}
