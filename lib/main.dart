593253551ec3e0b68814aaaaeb2943bf26fc58ae
Prateek-Prakash <63609044+Prateek-Prakash@users.noreply.github.com>
2022-04-09 20:28:52 -0400

    Update Theme

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,13 @@ class Application extends StatelessWidget {
     return MaterialApp(
       title: 'Calculator',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        fontFamily: 'Comfortaa',
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        brightness: Brightness.dark,
+        primaryColor: const Color(0xFF181A20),
+        canvasColor: const Color(0xFF181A20),
+        cardColor: const Color(0xFF262A34),
+        dialogBackgroundColor: const Color(0xFF262A34),
       ),
       home: const Calculator(),
     );
@@ -35,150 +41,152 @@ class _CalculatorState extends State<Calculator> {
       appBar: AppBar(
         title: const Text("Calculator"),
       ),
-      backgroundColor: Colors.white38,
-      body: Column(
-        children: [
-          Row(
-            children: [
-              CalcKey(
-                value: 'C',
-                flex: 1,
-                backgroundColor: Colors.blue.shade50,
-                foregroundColor: Colors.grey.shade900,
-              ),
-              CalcKey(
-                value: '±',
-                flex: 1,
-                backgroundColor: Colors.blue.shade50,
-                foregroundColor: Colors.grey.shade900,
-              ),
-              CalcKey(
-                value: '%',
-                flex: 1,
-                backgroundColor: Colors.blue.shade50,
-                foregroundColor: Colors.grey.shade900,
-              ),
-              CalcKey(
-                value: 'D',
-                flex: 1,
-                backgroundColor: Colors.blue.shade50,
-                foregroundColor: Colors.grey.shade900,
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              CalcKey(
-                value: '7',
-                flex: 1,
-                backgroundColor: Colors.white,
-                foregroundColor: Colors.grey.shade900,
-              ),
-              CalcKey(
-                value: '8',
-                flex: 1,
-                backgroundColor: Colors.white,
-                foregroundColor: Colors.grey.shade900,
-              ),
-              CalcKey(
-                value: '9',
-                flex: 1,
-                backgroundColor: Colors.white,
-                foregroundColor: Colors.grey.shade900,
-              ),
-              const CalcKey(
-                value: '÷',
-                flex: 1,
-                backgroundColor: Colors.blue,
-                foregroundColor: Colors.white,
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              CalcKey(
-                value: '4',
-                flex: 1,
-                backgroundColor: Colors.white,
-                foregroundColor: Colors.grey.shade900,
-              ),
-              CalcKey(
-                value: '5',
-                flex: 1,
-                backgroundColor: Colors.white,
-                foregroundColor: Colors.grey.shade900,
-              ),
-              CalcKey(
-                value: '6',
-                flex: 1,
-                backgroundColor: Colors.white,
-                foregroundColor: Colors.grey.shade900,
-              ),
-              const CalcKey(
-                value: 'x',
-                flex: 1,
-                backgroundColor: Colors.blue,
-                foregroundColor: Colors.white,
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              CalcKey(
-                value: '1',
-                flex: 1,
-                backgroundColor: Colors.white,
-                foregroundColor: Colors.grey.shade900,
-              ),
-              CalcKey(
-                value: '2',
-                flex: 1,
-                backgroundColor: Colors.white,
-                foregroundColor: Colors.grey.shade900,
-              ),
-              CalcKey(
-                value: '3',
-                flex: 1,
-                backgroundColor: Colors.white,
-                foregroundColor: Colors.grey.shade900,
-              ),
-              const CalcKey(
-                value: '–',
-                flex: 1,
-                backgroundColor: Colors.blue,
-                foregroundColor: Colors.white,
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              CalcKey(
-                value: '0',
-                flex: 1,
-                backgroundColor: Colors.white,
-                foregroundColor: Colors.grey.shade900,
-              ),
-              CalcKey(
-                value: '•',
-                flex: 1,
-                backgroundColor: Colors.white,
-                foregroundColor: Colors.grey.shade900,
-              ),
-              const CalcKey(
-                value: '=',
-                flex: 1,
-                backgroundColor: Colors.orange,
-                foregroundColor: Colors.white,
-              ),
-              const CalcKey(
-                value: '+',
-                flex: 1,
-                backgroundColor: Colors.blue,
-                foregroundColor: Colors.white,
-              ),
-            ],
-          ),
-        ],
+      body: Container(
+        padding: const EdgeInsets.all(5),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                CalcKey(
+                  value: 'C',
+                  flex: 1,
+                  backgroundColor: Colors.blue.shade100,
+                  foregroundColor: Colors.grey.shade900,
+                ),
+                CalcKey(
+                  value: '±',
+                  flex: 1,
+                  backgroundColor: Colors.blue.shade100,
+                  foregroundColor: Colors.grey.shade900,
+                ),
+                CalcKey(
+                  value: '%',
+                  flex: 1,
+                  backgroundColor: Colors.blue.shade100,
+                  foregroundColor: Colors.grey.shade900,
+                ),
+                const CalcKey(
+                  value: 'D',
+                  flex: 1,
+                  backgroundColor: Colors.red,
+                  foregroundColor: Colors.white,
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                CalcKey(
+                  value: '7',
+                  flex: 1,
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.grey.shade900,
+                ),
+                CalcKey(
+                  value: '8',
+                  flex: 1,
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.grey.shade900,
+                ),
+                CalcKey(
+                  value: '9',
+                  flex: 1,
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.grey.shade900,
+                ),
+                const CalcKey(
+                  value: '÷',
+                  flex: 1,
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.white,
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                CalcKey(
+                  value: '4',
+                  flex: 1,
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.grey.shade900,
+                ),
+                CalcKey(
+                  value: '5',
+                  flex: 1,
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.grey.shade900,
+                ),
+                CalcKey(
+                  value: '6',
+                  flex: 1,
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.grey.shade900,
+                ),
+                const CalcKey(
+                  value: 'x',
+                  flex: 1,
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.white,
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                CalcKey(
+                  value: '1',
+                  flex: 1,
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.grey.shade900,
+                ),
+                CalcKey(
+                  value: '2',
+                  flex: 1,
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.grey.shade900,
+                ),
+                CalcKey(
+                  value: '3',
+                  flex: 1,
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.grey.shade900,
+                ),
+                const CalcKey(
+                  value: '–',
+                  flex: 1,
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.white,
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                CalcKey(
+                  value: '0',
+                  flex: 1,
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.grey.shade900,
+                ),
+                CalcKey(
+                  value: '•',
+                  flex: 1,
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.grey.shade900,
+                ),
+                const CalcKey(
+                  value: '=',
+                  flex: 1,
+                  backgroundColor: Colors.orange,
+                  foregroundColor: Colors.white,
+                ),
+                const CalcKey(
+                  value: '+',
+                  flex: 1,
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.white,
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -201,9 +209,9 @@ class CalcKey extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: MediaQuery.of(context).size.width / 4 * flex,
+      width: (MediaQuery.of(context).size.width - 10) / 4 * flex,
       padding: const EdgeInsets.all(5),
-      height: MediaQuery.of(context).size.width / 4,
+      height: (MediaQuery.of(context).size.width - 10) / 4,
       child: ElevatedButton(
         style: ButtonStyle(
           backgroundColor: MaterialStateProperty.all<Color>(backgroundColor),
@@ -212,7 +220,9 @@ class CalcKey extends StatelessWidget {
           value,
           style: TextStyle(color: foregroundColor, fontSize: 25, fontWeight: FontWeight.bold),
         ),
-        onPressed: () {},
+        onPressed: () {
+          print('Key Pressed: $value');
+        },
       ),
     );
   }
