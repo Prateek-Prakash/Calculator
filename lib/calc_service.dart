3b2608d99387e070a972b320d49bd73de2b193ac
Prateek-Prakash <63609044+Prateek-Prakash@users.noreply.github.com>
2022-04-10 11:30:45 -0400

    Update CalcService

--- lib/calc_service.dart
+++ lib/calc_service.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/foundation.dart';
+
 class CalcService {
   static final CalcService _instance = CalcService._internal();
 
@@ -5,20 +7,43 @@ class CalcService {
     return _instance;
   }
 
+  double? a;
+  String? o;
+  double? b;
+
   CalcService._internal();
 
+  String calculate() {
+    switch (o) {
+      case "+":
+        return add(a!, b!).toString();
+      case "–":
+        return subtract(a!, b!).toString();
+      case "x":
+        return multiply(a!, b!).toString();
+      case "÷":
+        return divide(a!, b!).toString();
+      default:
+        return "Unsupported Operation";
+    }
+  }
+
+  @visibleForTesting
   double add(double a, double b) {
     return a + b;
   }
 
+  @visibleForTesting
   double subtract(double a, double b) {
     return a - b;
   }
 
+  @visibleForTesting
   double multiply(double a, double b) {
     return a * b;
   }
 
+  @visibleForTesting
   double divide(double a, double b) {
     return a / b;
   }
--- test/calc_service_test.dart
+++ test/calc_service_test.dart
@@ -2,23 +2,55 @@ import 'package:calculator/calc_service.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('Add Small Numbers', () {
-    double result = CalcService().add(5, 5);
-    expect(result, 10);
+  test('Calculate Add Operation', () {
+    CalcService().a = 5;
+    CalcService().o = "+";
+    CalcService().b = 5;
+    String result = CalcService().calculate();
+    expect(result, "10.0");
   });
 
-  test('Subtract Small Numbers', () {
-    double result = CalcService().subtract(5, 10);
-    expect(result, -5);
+  test('Calculate Subtract Operation', () {
+    CalcService().a = 5;
+    CalcService().o = "–";
+    CalcService().b = 10;
+    String result = CalcService().calculate();
+    expect(result, "-5.0");
   });
 
-  test('Multiply Small Numbers', () {
-    double result = CalcService().multiply(5, 5);
-    expect(result, 25);
+  test('Calculate Multiply Operation', () {
+    CalcService().a = 5;
+    CalcService().o = "x";
+    CalcService().b = 5;
+    String result = CalcService().calculate();
+    expect(result, "25.0");
   });
 
-  test('Divide Small Numbers', () {
-    double result = CalcService().divide(10, 5);
-    expect(result, 2);
+  test('Calculate Divide Operation', () {
+    CalcService().a = 10;
+    CalcService().o = "÷";
+    CalcService().b = 5;
+    String result = CalcService().calculate();
+    expect(result, "2.0");
+  });
+
+  test('Add Numbers Method', () {
+    double actual = CalcService().add(5, 5);
+    expect(actual, 10);
+  });
+
+  test('Subtract Numbers Method', () {
+    double actual = CalcService().subtract(5, 10);
+    expect(actual, -5);
+  });
+
+  test('Multiply Numbers Method', () {
+    double actual = CalcService().multiply(5, 5);
+    expect(actual, 25);
+  });
+
+  test('Divide Numbers Method', () {
+    double actual = CalcService().divide(10, 5);
+    expect(actual, 2);
   });
 }
